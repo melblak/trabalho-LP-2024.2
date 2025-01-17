6b2afd22861c624cadf209fbfe612ef857394664
mel blak <melinda.blak@gmail.com>
2025-01-17 03:28:49 -0300

    função média

--- main.hs
+++ main.hs
@@ -1,9 +1,9 @@
+import GHC.Exts.Heap (GenClosure(FloatClosure))
 -- Função que le linhas de um arquivo
 lerLinhas :: String -> IO [String] -- Entra uma o caminho do arquivo e retorna uma lista de strings
 lerLinhas arquivo = do
     conteudo <- readFile arquivo -- Lê o conteúdo do arquivo
     pure (lines conteudo) -- Encapsula o resultado em um IO
-
 
 
 -- Função que compara dois Strings
@@ -23,6 +23,15 @@ compLinhas (x:xs) (y:ys) =
     let resultado = compString x y -- Compara as duas strings
     in resultado : compLinhas xs ys -- Recursivamente compara o resto das strings e adiciona na lista
 compLinhas _ _ = [] -- Se uma lista acabar retorna uma lista vazia
+
+
+-- Função de saída
+resultados :: [Int] -> [Int] -> Float
+resultados [] [] = 0
+resultados (x:xs) (y:ys)
+    | x /= 0 = resultados xs ys
+    | otherwise = media x y
+    where media a b = fromIntegral (a) / fromIntegral b
 
 
 -- Função principal
--- teste.hs
+++ teste.hs
@@ -1,13 +1,5 @@
--- Função que compara dois Strings
-compString :: String -> String -> Int
-compString [] [] = 0  -- As duas strings estão vazias
-compString (x:xs) (y:ys)  -- Separa o primeiro caractere da string
-    | x == y    = compString xs ys  -- Se caracteres forem iguais, continua com o resto da string
-    | otherwise = 1 + compString xs ys  -- Se caracteres forem diferentes, soma 1 e continua
-compString [] ys = length ys -- Se a primeira string acabar e a segunda ainda tiver caracteres
-compString xs [] = length xs --Se a segunda string acabar e a primeira ainda tiver caracteres
-
-main = do
-    
-    let teste = compString "passaro" "passarinho"
-    print teste
+quantLinhas :: [Int] -> Int
+quantLinhas [] = 0
+quantLinhas (x:xs) 
+    | x - head xs /= 0 = 1 + quantLinhas xs
+    | otherwise = 0
