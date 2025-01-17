663df6ad42af73f402e2daf9e6a2017b38958a3b
Gabriel Braga <gabrielquintanilhabraga@hotmail.com>
2025-01-17 01:30:27 -0300

    v1

---
+++ main.hs
@@ -0,0 +1,40 @@
+-- Função que le linhas de um arquivo
+lerLinhas :: String -> IO [String] -- Entra uma o caminho do arquivo e retorna uma lista de strings
+lerLinhas arquivo = do
+    conteudo <- readFile arquivo -- Lê o conteúdo do arquivo
+    pure (lines conteudo) -- Encapsula o resultado em um IO
+
+
+
+-- Função que compara dois Strings
+compString :: String -> String -> Int
+compString [] [] = 0  -- As duas strings estão vazias
+compString (x:xs) (y:ys)  -- Separa o primeiro caractere da string
+    | x == y    = compString xs ys  -- Se caracteres forem iguais, continua com o resto da string
+    | otherwise = 1 + compString xs ys  -- Se caracteres forem diferentes, soma 1 e continua
+compString _ [] = 1 -- Se a primeira string acabar e a segunda ainda tiver caracteres
+compString [] _ = 1 -- Se a segunda string acabar e a primeira ainda tiver caracteres
+
+
+-- Função que compara as linhas
+compLinhas :: [String] -> [String] -> Int
+compLinhas [] [] = 0 -- As duas listas estão vazias
+compLinhas (x:xs) (y:ys) = do
+    let resultado = compString x y -- Compara as duas strings
+    let somatorio = []
+    somatorio ++ resultado
+    compLinhas xs ys -- Continua com o resto das listas
+
+
+-- Função principal
+main = do
+    -- Entrada dos arquivos
+    arq1 <- lerLinhas "arquivo1.txt"
+    arq2 <- lerLinhas "arquivo2.txt"
+
+
+    print arq1
+    print arq2
+
+    let nlinhas = compLinhas arq1 arq2
+    print nlinhas
---
+++ tempCodeRunnerFile.hs
@@ -0,0 +1,3 @@
+sumtorial :: Int -> Int
+sumtorial 0 = 0
+sumtorial n = n + sumtorial (n - 1)
